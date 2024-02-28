126bfdcf51bb3c184f14393733efa04f7598c78c
Azhar786786786 <talkazhar786@hotmail.com>
2024-02-28 14:24:20 +0530

    Added two files Footer.css, Footer.js

---
+++ src/Components/Footer.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div>
+        <h1>SUPPORT</h1>
+        <ul>
+          <li>Contact Us</li>
+          <li>Promotional & Sales</li>
+          <li>Track Order</li>
+          <li>Privacy Policy</li>
+          <li>Term & Conditions</li>
+          <li>Cookie Settings</li>
+        </ul>
+      </div>
+
+      <div>
+        <h1>FAQ</h1>
+        <ul>
+          <li>MyAccount</li>
+          <li>Return Policy</li>
+          <li>Tech Glossary</li>
+          <li>Initiate Return</li>
+        </ul>
+      </div>
+
+      <div>
+        <h1>ABOUT</h1>
+        <ul>
+          <li>Company</li>
+          <li>Corporate News</li>
+          <li>Press Center</li>
+          <li>Investors</li>
+          <li>Sustainability</li>
+          <li>Careers</li>
+          <li>Store Finder</li>
+          <li>Puma DNA</li>
+        </ul>
+      </div>
+
+      <div>
+        <h1>STAY UP TO DATE</h1>
+        <ul>
+          <li>YouTube</li>
+          <li>Twitter</li>
+          <li>Pintersons</li>
+          <li>Instagram</li>
+          <li>Facebook</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
