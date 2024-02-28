fc17fc753774f7c189cbe4af160acaf381d8208f
Azhar786786786 <talkazhar786@hotmail.com>
2024-02-28 15:52:07 +0530

    Added some value

--- src/Components/MainContent.js
+++ src/Components/MainContent.js
@@ -1,78 +1,21 @@
 import React from 'react';
 import './MainContent.css';
+import first from '../Assets/Images/Army-Trainer-Unisex-Sneakers.avif';
 
 function MainContent() {
   return (
     <div className="MainContent">
-      This is a tale of murder. Or maybe that's not quite true. At its heart, it's a love story, isn't it?
-
-      One spring morning, reclusive ex-movie star Lana Farrar invites a small group of her closest friends for a weekend away, on her small private island, just off the coast of Mykonos.
-
-      Beneath the surface, old friendships conceal violent passions and resentments. And in forty-eight hours, one of them will be dead.
-
-      But that was just the beginning...
-
-      You may think you know this story. Think again.
-
-      Cover image for Gone
-      Gone by Glenna Thomson
-      When Rebecca Bundy fails to return home after the last day of school in 1984 her father reports her missing. But the teenager has run away before and recently she's been bragging about going to Queensland, so the police tell the family to wait it out.
-
-      Days pass. Rumours swirl. A man seen loitering near the bus stop might have followed her. Was there something going on between Rebecca and a male teacher? What about the sheep farmer on Glen Lochan Road where she babysat? And why is her boyfriend, the rough cattle guy Bull Tennant, so sure something sinister has happened?
-
-      Then a shocking murder-suicide at a local farm diverts police attention and Rebecca's disappearance all too quickly becomes a cold case.
-
-      Cover image for The Consultant
-      The Consultant by Im Seong-sun & An Seon Jae (trans.)
-      The Consultant is very good at his job. He creates simple, elegant, effective solutions for – restructuring. Nothing obvious or messy. Certainly nothing anyone would ever suspect as murder. The 'natural deaths' he plans have always gone well – a medicine replaced here, a mechanism jammed there. His performance reviews are excellent. And it's not as though he knows these people.
-
-      Until his next 'customer' turns out to be someone he not only knows but cares about, and for the first time, he begins to question the role he plays in the vast, anonymous Company. And as he slowly begins to understand the real scope of their work, he realises just how easy it would be for the Company to arrange one more perfect murder. But how far will he go to escape The Company? And how far will they go to stop him?
-
-      Cover image for Argylle
-      Argylle by Elly Conway
-      A luxury train speeding towards Moscow and a date with destiny. A CIA plane downed in the jungles of the Golden Triangle. A Nazi hoard entombed in the remote mountains of South-West Poland. A missing treasure, the eighth wonder of the world, lost for seven decades.
-
-      One Russian magnate’s dream of restoring a nation to greatness has set in motion a chain of events which will take the world to the brink of chaos. Only Frances Coffey, the CIA’s most legendary spymaster, can prevent it. But to do so, she needs someone special. Enter Argylle, a troubled agent with a tarnished past who may just have the skills to take on one of the most powerful men in the world. If only he can save himself first…
-
-      READ REVIEW
-      Cover image for The Strip
-      The Strip by Iain Ryan
-      When a local doctor is brutally murdered, Detective Constable Lana Cohen joins Strike Force Diablo on the notorious Gold Coast, where she finds herself navigating a world of chaos and corruption.
-
-      As she delves deeper into the case, Lana reluctantly pairs up with Henry Loch, a detective with a tarnished reputation who sees an opportunity to redeem himself amidst the mayhem.
-
-      But as Lana and Henry unravel the threads of violence and deceit, they uncover a dark side to the Gold Coast that shocks them to the core. With six unsolved murders already haunting them, will they be able to untangle the web of lies before it's too late?
-
-      Cover image for The Concierge
-      The Concierge by Abby Corson
-      The peaceful setting of Cavengreen Hotel has been shattered by a shocking murder. Hector Harrow, the hotel's concierge, has been accused and is determined to clear his name.
-
-      Hector enlists the aid of Helen, a retired publisher, to document the shocking crime that has unfolded, with the intention of publishing a truthful account.
-
-      Amid interruptions from a nosy journalist and the egotistical antics of the hotel's new owner, American Dave, Helen works tirelessly to keep Hector focused on finishing his book. As suspicions fall on different guests and one makes a shocking confession, Hector must navigate a web of secrets to uncover the truth.
-
-      Cover image for The Silence in Her Eyes
-      The Silence in Her Eyes by Armando Lucas Correa
-      For as long as she can remember, twenty-eight-year-old Leah has suffered from akinetopsia – her eyes can't process movement, only still images. As she walks around her neighbourhood in Upper Manhattan, her white stick tapping the pavement, most people assume she's blind. But Leah sees a good deal, and she remembers every image her eyes capture.
-
-      Leah's routine is thrown out of whack when someone new moves in next door. Alice is beautiful and kind, but Leah can smell waves of anxiety coming off her. As they become friends Alice opens up about her abusive, alcoholic ex-husband, who refuses to let her go. Things take a turn for the worse when Leah hears Alice through the wall, begging for someone to help. With no one to call and moments to act, Leah is forced to make a decision that will test her courage, her strength and ultimately her sanity.
-
-      Cover image for Darkness Runs Deep
-      Darkness Runs Deep by Claire McNeel
-      In the darkest hour, a blood-soaked teenager flees the rural Gerandaroo football oval. Eight months later, Bess, a young teacher, returns home to Gerandaroo. A childhood game of dare with her former best friend forces Bess to form a women's footy team to play against Denby, a rival town. Bess reluctantly recruits players, but the team has to contend with hostile locals – including Bess's own father. Will this help the small community to come back together – or will it be the final thing that blows everything apart?
-
-      As tensions in the town boil over, so too do resentments and secrets and violence that have been previously held tight and close.
-
-      READ REVIEW
-      Cover image for The Fury
-      The Fury
-      Alex Michaelides
-
-      $34.99
-
-      ADD TO BAG
-      In stock at 8 shops, ships in 3-4 days
-
+      <div>
+        <h2>Trending Now</h2>
+        <div>
+          <img src={first} alt="sneeker" className="imgMain" />
+          {/* <div style={{ display: "flex" }}> */}
+          <div>
+            <p>Army-Trainer-Unisex-Sneakers</p>
+            <p>Rs - 6544</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
