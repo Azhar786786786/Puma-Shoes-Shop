d6bf3073c2a6109b263c7fab53fd2e5ede5c055a
Azhar786786786 <talkazhar786@hotmail.com>
2024-02-28 17:00:39 +0530

    Added new component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import './App.css';
 import Header from './Components/Header';
 import MainContent from './Components/MainContent';
 import Footer from './Components/Footer';
+import Child from './Components/Child';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <div style={{ display: 'flex' }}>
+      {/* <div style={{ display: 'flex', justifyContent: 'space-between', padding: '20px' }}>
         <MainContent />
         <MainContent />
         <MainContent />
         <MainContent />
         <MainContent />
         <MainContent />
-      </div>
+      </div> */}
+      <Child name="Azhar Hussain" name2="Qasim Khan" />
       <Footer />
     </div >
   );
