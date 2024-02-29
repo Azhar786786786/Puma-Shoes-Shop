3d408508bcbb76507c74f4cdfa1f78c2dd7d1f0b
Azhar786786786 <talkazhar786@hotmail.com>
2024-02-29 07:13:36 +0530

    Added child component data in the form of table

--- src/Components/Child.js
+++ src/Components/Child.js
@@ -4,8 +4,23 @@ function Child(props) {
   return (
     <div>
       <div>
-        <p>Hello {props.name}</p>
+        {/* <p>Hello {props.name}</p>
         <p>Hello {props.name2}</p>
+        <p>Hello {props.name3}</p> */}
+        <table border="1px">
+          <tr>
+            <th>Greeting</th>
+            <th>FirstName</th>
+            <th>MiddleName</th>
+            <th>LastName</th>
+          </tr>
+          <tr>
+            <td>Hello</td>
+            <td>{props.name}</td>
+            <td>{props.name2}</td>
+            <td>{props.name3}</td>
+          </tr>
+        </table>
       </div>
     </div>
   )
