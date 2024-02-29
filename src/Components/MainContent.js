0784e4c2b804c22229a0c82d5699e68cc386d2a0
Azhar786786786 <talkazhar786@hotmail.com>
2024-02-29 07:50:43 +0530

    Added props data

--- src/Components/MainContent.js
+++ src/Components/MainContent.js
@@ -2,17 +2,17 @@ import React from 'react';
 import './MainContent.css';
 import first from '../Assets/Images/Army-Trainer-Unisex-Sneakers.avif';
 
-function MainContent() {
+function MainContent(props) {
   return (
     <div className="MainContent">
       <div>
-        <h2>Trending Now</h2>
+        <h2>{props.heading}</h2>
         <div>
-          <img src={first} alt="sneeker" className="imgMain" />
+          <img src={props.imgdisp} alt="sneeker" className="imgMain" />
           {/* <div style={{ display: "flex" }}> */}
           <div>
-            <p>Army-Trainer-Unisex-Sneakers</p>
-            <p>Rs - 6544</p>
+            <p>{props.title}</p>
+            <p>Rs - {props.price}</p>
           </div>
         </div>
       </div>
