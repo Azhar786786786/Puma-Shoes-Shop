1a11407bad787b9e7778627a1c4cb15ee2a650db
Azhar786786786 <talkazhar786@hotmail.com>
2024-02-29 07:49:48 +0530

    Added MainContent component with attribute value

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Header from './Components/Header';
 import MainContent from './Components/MainContent';
 import Footer from './Components/Footer';
 import Child from './Components/Child';
+import first from './Assets/Images/Army-Trainer-Unisex-Sneakers.avif';
+import second from './Assets/Images/Blktop-Rider-Multicolor-Unisex-Sneakers.avif';
+import third from './Assets/Images/Exotek-NITRO™-Base-Unisex-Sneakers.avif';
 
 function App() {
   const name = {
@@ -13,16 +16,16 @@ function App() {
   return (
     <div className="App">
       <Header />
-      {/* <div style={{ display: 'flex', justifyContent: 'space-between', padding: '20px' }}>
-        <MainContent />
-        <MainContent />
-        <MainContent />
-        <MainContent />
-        <MainContent />
-        <MainContent />
-      </div> */}
-      <Child name={name.firstName} name2={name.MiddleName} name3={name.LastName} />
-      <Child name="Azhar Hussain" name2="Qasim Khan" />
+      <div style={{ display: 'flex', justifyContent: 'space-between', padding: '20px' }}>
+        <MainContent heading="Trending Now" title="Army-Trainer-Unisex-Sneakers" price="1000" imgdisp={first} />
+        <MainContent heading="Trending Now" title="Blktop-Rider-Multicolor-Unisex-Sneakers" price="2000" imgdisp={second} />
+        <MainContent heading="Trending Now" title="Exotek-NITRO™-Base-Unisex-Sneakers" price="3000" imgdisp={third} />
+        <MainContent heading="Trending Now" title="Army-Trainer-Unisex-Sneakers" price="1000" imgdisp={first} />
+        <MainContent heading="Trending Now" title="Blktop-Rider-Multicolor-Unisex-Sneakers" price="2000" imgdisp={second} />
+        <MainContent heading="Trending Now" title="Exotek-NITRO™-Base-Unisex-Sneakers" price="3000" imgdisp={third} />
+      </div>
+      {/* <Child name={name.firstName} name2={name.MiddleName} name3={name.LastName} />
+      <Child name="Azhar Hussain" name2="Qasim Khan" /> */}
       <Footer />
     </div >
   );
