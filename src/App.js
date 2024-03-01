2adf2e4bbe8e9a3f0c97426479d2fbab09a5d179
Azhar786786786 <talkazhar786@hotmail.com>
2024-03-01 08:35:05 +0530

    App distribution

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from './Components/Header';
-import ShoeCentre from './src/Components/ShoeCentre';
+import ShoeCentre from './Components/ShoeCentre';
 import Footer from './Components/Footer';
 
 
